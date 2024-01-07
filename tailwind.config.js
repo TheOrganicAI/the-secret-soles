2e96d235573b1d636632f5f7728abb4da02efe0b
TheOrganicAI <aylacroft@proton.me>
2024-01-07 18:16:56 -0500

    suck my toes!!!! It's going down.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.js',
+    // Add paths to all other HTML/JS files.
+  ],
+  darkMode: 'class', // Enable dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit', // Just In Time Compiler
+}
